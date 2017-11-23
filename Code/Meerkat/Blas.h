f1b051b4cb2222a4071bfb798d22c612665cfd76
sunnior <sunnior@gmail.com>
2017-11-23 21:57:08 +0800

    [*] clean up code.

--- Code/Meerkat/Blas.h
+++ Code/Meerkat/Blas.h
@@ -1,6 +1,7 @@
 #ifndef __MEERKAT_BLAS_H__
 #define __MEERKAT_BLAS_H__
 #include "cblas.h"
+#include <cstring>
 
 namespace DeepLearning
 {
@@ -9,15 +10,25 @@ namespace DeepLearning
 	extern cblas_sgemm_type cblas_sgemm_ptr;
 
 	template<typename T>
-	void dl_gemm(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-		OPENBLAS_CONST T alpha, OPENBLAS_CONST T *A, OPENBLAS_CONST blasint lda, OPENBLAS_CONST T *B, OPENBLAS_CONST blasint ldb, OPENBLAS_CONST T beta, T *C, OPENBLAS_CONST blasint ldc);
+	void dl_gemm_cpu(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const blasint M, const blasint N, const blasint K,
+		const T alpha, const T *A, const blasint lda, const T *B, const blasint ldb, const T beta, T *C, const blasint ldc);
 
 	template<>
 	inline
-	void dl_gemm<float>(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-		OPENBLAS_CONST float alpha, OPENBLAS_CONST float *A, OPENBLAS_CONST blasint lda, OPENBLAS_CONST float *B, OPENBLAS_CONST blasint ldb, OPENBLAS_CONST float beta, float *C, OPENBLAS_CONST blasint ldc)
+	void dl_gemm_cpu<float>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const blasint M, const blasint N, const blasint K,
+		const float alpha, const float *A, const blasint lda, const float *B, const blasint ldb, const float beta, float *C, const blasint ldc)
 	{
 		cblas_sgemm_ptr(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc);
+	}
+
+	template<typename T>
+	void dl_memcpy_cpu(T* dest, const T* src, const blasint size, const blasint repeat)
+	{
+		for (blasint i = 0; i < repeat; ++i)
+		{
+			std::memcpy(dest, src, sizeof(T)*size);
+			dest += size;
+		}
 	}
 
 	void InitBlas();
--- Code/Meerkat/LinearLayer.cpp
+++ Code/Meerkat/LinearLayer.cpp
@@ -43,35 +43,17 @@ namespace DeepLearning
 		input_data[1] = 2.0f;
 		weight_data[0] = 2.0f;
 		weight_data[1] = 3.0f;
-		
+		bias_data[0] = 0.5;
 
-		dl_gemm<dl_tensor>(CblasRowMajor, CblasNoTrans, CblasTrans,
+		dl_memcpy_cpu<dl_tensor>(output_data, bias_data, n_size, m_size);
+
+		dl_gemm_cpu<dl_tensor>(CblasRowMajor, CblasNoTrans, CblasTrans,
 			m_size, n_size, k_size,
 			1.0f, input_data, k_size,
 			weight_data, k_size,
-			0.0f, output_data, m_size);
-
-			//typedef void(*cblas_sgemm_type)(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K,
-				//OPENBLAS_CONST float alpha, OPENBLAS_CONST float *A, OPENBLAS_CONST blasint lda, OPENBLAS_CONST float *B, OPENBLAS_CONST blasint ldb, OPENBLAS_CONST float beta, float *C, OPENBLAS_CONST blasint ldc);
-
-			/*
-		dl_uint32 d1 = m_weight->GetShape(0);
-		dl_uint32 d2 = m_weight->GetShape(1);
+			1.0f, output_data, m_size);
 
 
-		dl_tensor* input_data = input->GetCpuData();
-		dl_tensor* output_data = output->GetCpuData();
-		dl_tensor* weight_data = m_weight->GetCpuData();
-		dl_tensor* bias_data = m_bias->GetCpuData();
-
-		for (dl_uint32 i = 0; i < d2; ++i)
-		{
-			for (dl_uint32 j = 0; j < d1; ++j)
-			{
-				output_data[i] += input_data[i*d1 + j] * weight_data[i*d1 + j];
-			}
-			output_data[i] += bias_data[i];
-		}*/
 	}
 
 	void LinearLayer::_ForwardGpu(Tensor* input, Tensor* output)
